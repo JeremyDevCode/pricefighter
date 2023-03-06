686cf8df1a0e0f5884e7fe032a6e56a3776002db
JeremyDevCode <jeremydevcode@gmail.com>
2023-03-06 17:51:24 -0500

    fixing

--- src/icons/Minimize.jsx
+++ src/icons/Minimize.jsx
@@ -10,9 +10,9 @@ function Minimize({ className, size = 24 }) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path d="M8 3v3a2 2 0 0 1-2 2H3"></path>
       <path d="M21 8h-3a2 2 0 0 1-2-2V3"></path>
