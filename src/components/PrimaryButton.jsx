9e3bb9a8eb1ce538e09c42c6d321cebdbd4761b8
JeremyDevCode <jeremydevcode@gmail.com>
2023-03-06 15:50:44 -0500

    Fixing linter errors

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-function PrimaryButton({children}) {
+function PrimaryButton({ children }) {
   return (
-    <button className='w-[220px] h-[60px] bg-[#14FF00] rounded-[30px]'>
-        <h2 className='text-white text-[26px]'>
-            {children}
-        </h2>
+    <button className="w-[220px] h-[60px] bg-[#14FF00] rounded-[30px]">
+      <h2 className="text-white text-[26px]">{children}</h2>
     </button>
   )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,7 @@
 export default function Home() {
-    return (
-      <main>
-        <h1>About</h1>
-      </main>
-    )
-  }
-  
+  return (
+    <main>
+      <h1>About</h1>
+    </main>
+  )
+}
