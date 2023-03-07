9d29fffc8558f9c983ca999d1bfef75c68ff0b4a
Daniel Gómez <51272519+devenapuros@users.noreply.github.com>
2023-03-06 19:03:55 -0600

    Fix layout on game page

--- src/components/Versus.jsx
+++ src/components/Versus.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Versus() {
   return (
-    <div className="flex items-center justify-center absolute top-0 left-0 right-0 bottom-0 m-auto w-[115px] h-[115px] bg-white rounded-full z-50">
-      <h2 className="text-5xl font-bold text-[#444444]">VS</h2>
+    <div className="flex items-center justify-center absolute top-0 left-0 right-0 bottom-0 m-auto w-[80px] h-[80px] bg-white rounded-full z-50">
+      <h2 className="text-2xl font-bold text-[#111]">VS</h2>
     </div>
   )
 }
