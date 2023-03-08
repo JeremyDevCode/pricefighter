6c2bff8b56d0dcfb0e878012dac876825d8da172
JeremyDevCode <jeremydevcode@gmail.com>
2023-03-08 13:13:08 -0500

    Active WinModal

--- src/pages/game.js
+++ src/pages/game.js
@@ -8,6 +8,7 @@ import { useAuth } from '../context/AuthContext'
 import { ProductCard } from '../components/ProductCard'
 import { useSoundEffects } from '../context/SoundEffectsContext'
 import { FailModal } from '../components/FailModal'
+import { WinModal } from '../components/WinModal'
 
 function shuffle(arra1) {
   let ctr = arra1.length
@@ -32,6 +33,7 @@ export default function Home() {
   const { play } = useSoundEffects()
   const carouselRef = useRef()
   const [fail, setFail] = useState(false)
+  const [win, setWin] = useState(false)
 
   useEffect(() => {
     const getData = async () => {
@@ -98,6 +100,11 @@ export default function Home() {
           <FailModal
             modalVisible={fail}
             setModalVisible={(val) => setFail(val)}
+            currentScore={score}
+          />
+          <WinModal
+            modalVisible={win}
+            setModalVisible={score > 18 && setWin(true)}
             currentScore={score}
           />
         </>
