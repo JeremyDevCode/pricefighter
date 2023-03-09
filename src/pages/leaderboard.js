53da5074758867e8f11ae263defaf51d3e8a52b3
ChermDev <carlos_e8246@hotmail.com>
2023-03-09 01:11:57 -0600

    Add loading msg to leaderboard

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -9,31 +9,37 @@ function leaderboard() {
   const [boardlist, setBoardlist] = useState([])
   const [gameMissing, setGameMissing] = useState(false)
   const [currentRanking, setCurrentRanking] = useState(null)
+  const [isLoadingUserRank, setLoadingUserRank] = useState(true)
+  const [isLoadingRanking, setLoadingRanking] = useState(true)
   const { auth } = useAuth()
 
   useEffect(() => {
     const getData = async () => {
       try {
+        setLoadingRanking(true)
         let response = await fetch(
           'https://hackafor-api.up.railway.app/ranking/'
         )
         const data = await response.json()
         setBoardlist(data)
+        setLoadingRanking(false)
         if (auth) {
+          setLoadingUserRank(true)
           response = await fetch(
             'https://hackafor-api.up.railway.app/user/ranking/',
             {
               headers: {
-                Authorization: `Bearer ${auth.accessToken}`
+                Authorization: `Bearer ${auth.accessToken}`,
+                'Content-Type': 'application/json'
               }
             }
           )
           const data = await response.json()
-          console.log(data)
           if (data.score || data.rank) {
             setCurrentRanking({ score: data.score, rank: data.rank })
             setGameMissing(false)
           } else setGameMissing(true)
+          setLoadingUserRank(false)
         }
       } catch (error) {
         console.log(error)
@@ -50,43 +56,60 @@ function leaderboard() {
           <h2 className="text-2xl font-semibold text-white">You</h2>
           <small className="font-semibold text-gray-500 mr-6">Score</small>
         </div>
-        {auth && currentRanking ? (
-          <ScoreCard
-            top={currentRanking.rank}
-            name={auth?.userName}
-            avatar={auth?.userAvatar}
-            score={currentRanking.score}
-          />
-        ) : gameMissing ? (
-          <h1 className="text-gray-500 font-semibold my-6">
-            Please play a game to see your score
-          </h1>
-        ) : (
-          <h1 className="text-gray-500 font-semibold my-6">
-            Please login to view your ranking
-          </h1>
-        )}
+        {
+          isLoadingUserRank ?
+            (
+              <h1 className="text-gray-500 font-semibold my-6">
+                Loading...
+              </h1>
+            ) :
+            (auth && currentRanking ? (
+              <ScoreCard
+                top={currentRanking.rank}
+                name={auth?.userName}
+                avatar={auth?.userAvatar}
+                score={currentRanking.score}
+              />
+            ) : gameMissing ? (
+              <h1 className="text-gray-500 font-semibold my-6">
+                Please play a game to see your score
+              </h1>
+            ) : (
+              <h1 className="text-gray-500 font-semibold my-6">
+                Please login to view your ranking
+              </h1>
+            ))
+        }
         <div className="flex items-start justify-start w-full py-5">
           <h2 className="text-2xl font-semibold text-white">Leaderboard</h2>
         </div>
-        {boardlist &&
-          boardlist.length > 0 &&
-          boardlist.map((item) => (
-            <ScoreCard
-              key={item.id}
-              top={item.rank}
-              name={item.data?.user_metadata?.user_name}
-              avatar={item.data?.user_metadata?.avatar_url}
-              score={item.score}
-            />
-          ))}
-        {(!boardlist || boardlist.length === 0) && (
-          <div>
+        {isLoadingRanking ?
+          (
             <h1 className="text-gray-500 font-semibold">
-              No hay usuarios para mostrar
+              Loading
             </h1>
-          </div>
-        )}
+          ) :
+          (
+            (!boardlist || boardlist.length === 0) ?
+              (
+                <div>
+                  <h1 className="text-gray-500 font-semibold">
+                    No users to display
+                  </h1>
+                </div>
+              ) :
+              boardlist &&
+              boardlist.length > 0 &&
+              boardlist.map((item) => (
+                <ScoreCard
+                  key={item.id}
+                  top={item.rank}
+                  name={item.data?.user_metadata?.user_name}
+                  avatar={item.data?.user_metadata?.avatar_url}
+                  score={item.score}
+                />
+              ))
+          )}
       </div>
     </section>
   )
